1a1b256504239c1a08786911d738c22f8ee9beaf
Jose Medina <116831948+jomedinagomez@users.noreply.github.com>
2023-04-27 19:13:10 -0400

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -13,8 +13,7 @@ const EXAMPLES: ExampleModel[] = [
         value: "What is included in my Northwind Health Plus plan that is not in standard?"
     },
     { text: "how many stocks did microsoft repurchased in 2023?", value: "how many stocks did microsoft repurchased in 2023?" },
-    { text: "What was Microsoft’s revenue for the nine months that ended on March 31 2023?", value: "What was Microsoft’s revenue for the nine months that ended on March 31 2023?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What was Microsoft’s revenue for the nine months that ended on March 31 2023?", value: "What was Microsoft’s revenue for the nine months that ended on March 31 2023?" }
 ];
 
 interface Props {
